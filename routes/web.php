5d336c91af6490ac95b7c9789f13c97837437370
alumno <alumno@vm1>
2020-11-18 09:03:30 +0100

    Solución parcial a los ejercicios I

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Pantalla principal';
+});
+
+Route::get('login', function () {
+    return 'Login usuario';
 });
