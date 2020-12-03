5323b033edd0b81e8696f029cd3a67831ca6d3d8
alumno <plajommj@hotmail.com>
2020-12-03 09:53:27 +0100

    Copiado muchas cosas del profe

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,35 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Models\Movie;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function getIndex()
+    {
+		return view('catalog.index', array('arrayPeliculas' => Movie::all()));
+    }
+
+    public function getShow($id)
+    {
+    	return view('catalog.show',
+    		array(
+    			'pelicula' => $this->arrayPeliculas[$id],
+    			'id' => $id
+    		)
+    	);
+    }
+
+    public function getCreate()
+    {
+    	return view('catalog.create');
+    }
+
+    public function getEdit($id)
+    {
+    	return view('catalog.edit', array( 'pelicula' => Movie::FindorFail($id)));
+    }
+
 }
--- resources/views/catalog/edit.blade.php
+++ resources/views/catalog/edit.blade.php
@@ -2,6 +2,57 @@
 
 @section('content')
 
-    Modificar película {{ $id }}
+<div class="row" style="margin-top:40px">
+   <div class="offset-md-3 col-md-6">
+      <div class="card">
+         <div class="card-header text-center">
+            Modificar película
+         </div>
+         <div class="card-body" style="padding:30px">
+
+            <form action="{{ url('/catalog/edit') }}" method="POST">
+            	{{method_field('PUT')}}
+
+	            @csrf
+
+                <input type="hidden" name="id" value="{{ $pelicula->id }}">
+
+	            <div class="form-group">
+	               <label for="title">Título</label>
+                <input type="text" name="title" id="title" value="{{ $pelicula->title }}" class="form-control">
+	            </div>
+
+	            <div class="form-group">
+	            	<label for="title">A&ntilde;o</label>
+	               <input type="number" name="year" id="year" value="{{$pelicula->year}}">
+	            </div>
+
+	            <div class="form-group">
+	            	<label for="title">Director</label>
+	               <input type="text" name="director" id="director" class="form-control" value="{{$pelicula->director}}">
+	            </div>
+
+	            <div class="form-group">
+	            	<label for="title">P&oacute;ster</label>
+	               <input type="text" name="poster" id="poster" class="form-control" value="{{$pelicula->director}}">
+	            </div>
+
+	            <div class="form-group">
+	               <label for="synopsis">Resumen</label>
+	               <textarea name="synopsis" id="synopsis" class="form-control" rows="3">{{$pelicula->synopsis}}</textarea>
+	            </div>
+
+	            <div class="form-group text-center">
+	               <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+	                   Modificar película
+	               </button>
+	            </div>
+
+            </form>
+
+         </div>
+      </div>
+   </div>
+</div>
 
 @stop
