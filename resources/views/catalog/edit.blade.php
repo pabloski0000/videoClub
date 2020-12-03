8d60ecf7e25836f27770aa6a1c46f297d0f9e7e0
Alberto Sierra <alberto.sierra@murciaeduca.es>
2020-12-03 09:40:13 +0100

    Final de la tercera tanda de ejercicios

--- resources/views/catalog/edit.blade.php
+++ resources/views/catalog/edit.blade.php
@@ -2,10 +2,6 @@
 
 @section('content')
 
-@extends('layouts.master')
-
-@section('content')
-
 <div class="row" style="margin-top:40px">
    <div class="offset-md-3 col-md-6">
       <div class="card">
@@ -19,29 +15,31 @@
 
 	            @csrf
 
+	            <input type="hidden" name="id" id="id" value="{{ $pelicula->id }}">
+
 	            <div class="form-group">
 	               <label for="title">Título</label>
-	               <input type="text" name="title" id="title" class="form-control">
+	               <input type="text" name="title" id="title" value="{{ $pelicula->title }}" class="form-control">
 	            </div>
 
 	            <div class="form-group">
-	            	<label for="title">A&ntilde;o</label>
-	               <input type="number" name="year" id="year">
+	            	<label for="year">A&ntilde;o</label>
+	               <input type="number" name="year" id="year" value="{{ $pelicula->year }}">
 	            </div>
 
 	            <div class="form-group">
-	            	<label for="title">Director</label>
-	               <input type="text" name="director" id="director" class="form-control">
+	            	<label for="director">Director</label>
+	               <input type="text" name="director" id="director" value="{{ $pelicula->director }}" class="form-control">
 	            </div>
 
 	            <div class="form-group">
-	            	<label for="title">P&oacute;ster</label>
-	               <input type="text" name="poster" id="poster" class="form-control">
+	            	<label for="poster">P&oacute;ster</label>
+	               <input type="text" name="poster" id="poster" value="{{ $pelicula->poster }}" class="form-control">
 	            </div>
 
 	            <div class="form-group">
 	               <label for="synopsis">Resumen</label>
-	               <textarea name="synopsis" id="synopsis" class="form-control" rows="3"></textarea>
+	               <textarea name="synopsis" id="synopsis" class="form-control" rows="3">{{ $pelicula->synopsis }}</textarea>
 	            </div>
 
 	            <div class="form-group text-center">
@@ -56,8 +54,5 @@
       </div>
    </div>
 </div>
-
-@stop
-
 
 @stop
