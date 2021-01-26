3466e4acab428846de170b22cdf53c2e2ac6bc12
alumno <plajommj@hotmail.com>
2021-01-26 12:29:47 +0100

    CRUD beta

---
+++ app/Http/Controllers/API/PeliculaController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Pelicula;
+use Illuminate\Http\Request;
+use App\Http\Resources\PeliculaResource;
+
+class PeliculaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new PeliculaResource(Pelicula::paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $pelicula = Pelicula::create(json_decode($request->getContent(), true));
+
+        return new PeliculaResource($pelicula);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pelicula $pelicula)
+    {
+        return new PeliculaResource($pelicula);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pelicula $pelicula)
+    {
+        $pelicula->update(json_decode($request->getContent(), true));
+
+        return new PeliculaResource($pelicula);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pelicula  $pelicula
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pelicula $pelicula)
+    {
+        $pelicula->delete();
+    }
+}
---
+++ database/migrations/2021_01_26_110311_create_peliculas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePeliculasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('peliculas', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->year('year')->nullable();
+            $table->string('director', 64);
+            $table->string('poster')->nullable();
+            $table->boolean('rented')->default(false);
+            $table->text('synopsis')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('peliculas');
+    }
+}
