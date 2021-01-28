86fd934997b5ee57216776526b4e67c56e43d7ed
alumno <plajommj@hotmail.com>
2021-01-28 10:29:21 +0100

    CRUD protegido

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
 use App\Models\User;
+use GuzzleHttp\Middleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,10 +21,16 @@ use App\Models\User;
 
 Route::post('/tokens/create', [LoginController::class, 'createToken']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
-Route::apiResource('peliculas', PeliculaController::class);
+/*Route::middleware('auth:sanctum')->get('/api', function (Request $request) {
+    return $request->user();
+});*/
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::apiResource('peliculas', PeliculaController::class);
+});
 
 Route::get('peliculas/search/{search}', [PeliculaController::class, 'search']);
